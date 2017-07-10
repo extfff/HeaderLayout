1f4bf8bc47f8c32aaa7d74441c3a6d5a48767cc7
vendor <vendor_api@qq.com>
2017-07-10 17:42:24 +0800

    初始化版本

---
+++ app/src/main/java/com/vendor/sample/MainActivity.java
@@ -0,0 +1,28 @@
+package com.vendor.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.edit_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //...
+            }
+        });
+
+        findViewById(R.id.add_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //...
+            }
+        });
+    }
+}
